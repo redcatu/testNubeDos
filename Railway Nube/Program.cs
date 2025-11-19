10bad8b728b9279f2aca1da17f02c27e97a6c372
unknown <melanyboxig@gmail.com>
2025-11-19 10:24:06 -0400

    primer commit

---
+++ Railway Nube/Program.cs
@@ -0,0 +1,21 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.WebHost.UseUrls("https://localhost:7096");
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
